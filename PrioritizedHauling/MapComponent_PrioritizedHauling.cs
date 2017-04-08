3891638be6d28c2dc0f290ac70e8b50866ca0fbd
maarxx <chris.lewark@gmail.com>
2017-04-08 13:14:40 -0400

    Code

---
+++ PrioritizedHauling/MapComponent_PrioritizedHauling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace PrioritizedHauling
+{
+    class MapComponent_PrioritizedHauling : MapComponent
+    {
+        public MapComponent_PrioritizedHauling(Map map) : base(map)
+        {
+        }
+    }
+}
